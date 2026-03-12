7c6d8d6655432da17249dd921de5122fc6a242ec
Edmondchong <140604478+Edmondchong@users.noreply.github.com>
2026-03-12 15:05:15 +0800

    123

--- products.py
+++ products.py
@@ -3,6 +3,10 @@ products = {
     "Power List": [
         {"name": "1.5mm_2Gang", "image": "image/1.5mm_2Gang.jpg"},
         {"name": "1.5mm_Commando", "image": "image/1.5mm_Commando.jpg"},
+        {"name": "3.5mm_Commando", "image": "image/1.5mm_Commando.jpg"},
+        {"name": "4.5mm_Commando", "image": "image/1.5mm_Commando.jpg"},
+        {"name": "5.5mm_Commando", "image": "image/1.5mm_Commando.jpg"},
+        {"name": "6.5mm_Commando", "image": "image/1.5mm_Commando.jpg"},
     ],
 
     "Audio List": [
