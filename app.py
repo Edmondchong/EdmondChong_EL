9e65353ea153f904a52a24b9bbed50a3d1a784bf
Edmondchong <140604478+Edmondchong@users.noreply.github.com>
2026-03-20 13:43:37 +0800

    image

--- app.py
+++ app.py
@@ -246,7 +246,7 @@ for category, product_list in products.items():
                             align-items:center;
                             justify-content:center;
                             text-align:center;
-                            padding-top:-15px;
+                            padding-top:-55px;
                             margin-bottom:5px;
                         ">
                         {product['name'].replace("_"," ")}
